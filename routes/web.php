2043bbf40eda6da95beba9e58059cbdc2a42264d
dayat <hidayatachmad316@gmail.com>
2026-06-25 20:03:23 +0800

    reset

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
-    Route::livewire('categories', 'pages::category.index')
-->name('category.index');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-
-require __DIR__.'/settings.php';
